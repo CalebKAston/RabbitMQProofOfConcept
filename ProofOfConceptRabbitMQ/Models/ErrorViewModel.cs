8eba363f0c1b20d9524d3c2c83257a08c65d6a35
Caleb Aston <CAston@mindfiretechnology.com>
2018-12-14 10:19:06 -0700

    Add project files.

---
+++ ProofOfConceptRabbitMQ/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProofOfConceptRabbitMQ.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
